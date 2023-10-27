60a2a55cbd4d88000c3a0f7522402b64c065b92f
MonkaS <danchik646@gmail.com>
2023-10-27 21:51:43 +0300

    get search input field in view + show it on index

--- hih2023/search/views.py
+++ hih2023/search/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
+    context = {'docs': None}
     template = 'search/index.html'
-    return render(request, template)
+    if request.method == "POST":
+        form = request.POST.dict()
+        context['docs'] = form['input_field']
+        # print(form['input_field'].value())
+        print(form['input_field'])
+        return render(request, template, context)
+    else:
+        return render(request, template)
