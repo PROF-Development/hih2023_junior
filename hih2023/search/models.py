ea2e65d7cdf5d680f61b57ef1e727aab9b1f3aec
Danil <112985637+dsnvel@users.noreply.github.com>
2023-10-29 06:37:15 +0300

    Last version

--- hih2023/search/models.py
+++ hih2023/search/models.py
@@ -3,9 +3,11 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roles = models.ManyToManyField('Role')
+
 
 class Role(models.Model):
     name = models.CharField(max_length=64)
